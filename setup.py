96855e18eefa1c2427a84aa8ed0dde685577d7d9
Mohammed Ismail <mohammedismail523261@gmail.com>
2024-06-05 19:55:26 +0530

    added basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='policyengine',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'policyengine_us',
+        'policyengine_uk',
+        #'policyengine_canada'
+    ],
+)
