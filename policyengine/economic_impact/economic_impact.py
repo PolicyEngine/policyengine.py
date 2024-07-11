f8c29f89ba1177fa540b5802580566e5fb477280
Mohammed Ismail <mohammedismail523261@gmail.com>
2024-07-11 13:20:19 +0530

    added classes for overall & edited the main class

--- policyengine/economic_impact/economic_impact.py
+++ policyengine/economic_impact/economic_impact.py
@@ -36,6 +36,12 @@ from .budgetary_impact.by_program.by_program import (
     PensionCredit
 )
 
+from .budgetary_impact.overall.overall import (
+    BudgetaryImpact, 
+    BenefitSpendingImpact, 
+    TaxRevenueImpact
+)
+
 from typing import Dict
 
 class EconomicImpact:
@@ -69,6 +75,9 @@ class EconomicImpact:
 
         # Set up metric calculators
         self.metric_calculators: Dict[str, object] = {
+            "budgetary/overall/budgetary_impact" : BudgetaryImpact(self.baseline, self.reformed),
+            "budgetary/overall/benefit_spending_impact" : BenefitSpendingImpact(self.baseline, self.reformed),
+            "budgetary/overall/tax_revenue_impact" : TaxRevenueImpact(self.baseline, self.reformed),
             "budgetary/by_program/income_tax" : IncomeTax(self.baseline, self.reformed),
             "budgetary/by_program/national_insurance" : NationalInsurance(self.baseline, self.reformed),
             "budgetary/by_program/vat" : Vat(self.baseline, self.reformed),
