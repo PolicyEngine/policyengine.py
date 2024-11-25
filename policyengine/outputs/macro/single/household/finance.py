9fab17d46ac6ddb41e82840b4fed85fc26c8b4ca
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2024-11-25 10:49:16 +0000

    Add budgetary impact comparison logic

---
+++ policyengine/outputs/macro/single/household/finance.py
@@ -0,0 +1,65 @@
+from policyengine import Simulation
+
+
+def finance(simulation: Simulation) -> dict:
+    sim = simulation.baseline
+
+    total_net_income = sim.calculate("household_net_income").sum()
+    employment_income_hh = (
+        sim.calculate("employment_income", map_to="household")
+        .astype(float)
+        .tolist()
+    )
+    self_employment_income_hh = (
+        sim.calculate("self_employment_income", map_to="household")
+        .astype(float)
+        .tolist()
+    )
+    household_net_income = (
+        sim.calculate("household_net_income").astype(float).tolist()
+    )
+    equiv_household_net_income = (
+        sim.calculate("equiv_household_net_income").astype(float).tolist()
+    )
+    household_income_decile = (
+        sim.calculate("household_income_decile").astype(int).tolist()
+    )
+    household_market_income = (
+        sim.calculate("household_market_income").astype(float).tolist()
+    )
+    if "total_wealth" in sim.tax_benefit_system.variables:
+        wealth = sim.calculate("total_wealth")
+        household_count_people = sim.calculate("household_count_people").values
+        wealth.weights *= household_count_people
+        wealth_decile = wealth.decile_rank().clip(1, 10).astype(int).tolist()
+        wealth = wealth.astype(float).tolist()
+    else:
+        wealth = None
+        wealth_decile = None
+
+    in_poverty = sim.calculate("in_poverty").astype(bool).tolist()
+    person_in_poverty = (
+        sim.calculate("in_poverty", map_to="person").astype(bool).tolist()
+    )
+    person_in_deep_poverty = (
+        sim.calculate("in_deep_poverty", map_to="person").astype(bool).tolist()
+    )
+    poverty_gap = sim.calculate("poverty_gap").sum()
+    deep_poverty_gap = sim.calculate("deep_poverty_gap").sum()
+
+    return {
+        "total_net_income": total_net_income,
+        "employment_income_hh": employment_income_hh,
+        "self_employment_income_hh": self_employment_income_hh,
+        "household_net_income": household_net_income,
+        "equiv_household_net_income": equiv_household_net_income,
+        "household_income_decile": household_income_decile,
+        "household_market_income": household_market_income,
+        "wealth": wealth,
+        "wealth_decile": wealth_decile,
+        "in_poverty": in_poverty,
+        "person_in_poverty": person_in_poverty,
+        "person_in_deep_poverty": person_in_deep_poverty,
+        "poverty_gap": poverty_gap,
+        "deep_poverty_gap": deep_poverty_gap,
+    }
---
+++ policyengine/outputs/macro/single/household/labor_supply.py
@@ -0,0 +1,62 @@
+from policyengine import Simulation
+
+
+def labor_supply(simulation: Simulation) -> dict:
+    sim = simulation.baseline
+    household_count_people = sim.calculate("household_count_people").values
+    result = {
+        "substitution_lsr": 0,
+        "income_lsr": 0,
+        "budgetary_impact_lsr": 0,
+        "income_lsr_hh": (household_count_people * 0).astype(float).tolist(),
+        "substitution_lsr_hh": (household_count_people * 0)
+        .astype(float)
+        .tolist(),
+        "weekly_hours": 0,
+        "weekly_hours_income_effect": 0,
+        "weekly_hours_substitution_effect": 0,
+    }
+
+    if not has_behavioral_response(simulation):
+        return result
+
+    result.update(
+        {
+            "substitution_lsr": sim.calculate(
+                "substitution_elasticity_lsr"
+            ).sum(),
+            "income_lsr": sim.calculate("income_elasticity_lsr").sum(),
+            "income_lsr_hh": sim.calculate(
+                "income_elasticity_lsr", map_to="household"
+            )
+            .astype(float)
+            .tolist(),
+            "substitution_lsr_hh": sim.calculate(
+                "substitution_elasticity_lsr", map_to="household"
+            )
+            .astype(float)
+            .tolist(),
+        }
+    )
+
+    if simulation.country == "us":
+        result.update(
+            {
+                "weekly_hours": sim.calculate("weekly_hours_worked").sum(),
+                "weekly_hours_income_effect": sim.calculate(
+                    "weekly_hours_worked_behavioural_response_income_elasticity"
+                ).sum(),
+                "weekly_hours_substitution_effect": sim.calculate(
+                    "weekly_hours_worked_behavioural_response_substitution_elasticity"
+                ).sum(),
+            }
+        )
+
+
+def has_behavioral_response(simulation):
+    sim = simulation.baseline
+    return (
+        "employment_income_behavioral_response"
+        in sim.tax_benefit_system.variables
+        and any(sim.calculate("employment_income_behavioral_response") != 0)
+    )
