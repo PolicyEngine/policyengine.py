d6309f25fb766a7f91b71f6a9aaf11e1f59f306e
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-02-20 11:34:45 +0000

    Remove old files

--- policyengine/outputs/macro/single/calculate_average_earnings.py
+++
@@ -1,9 +0,0 @@
-from policyengine import Simulation
-
-
-def calculate_average_earnings(simulation: Simulation) -> float:
-    """Calculate average earnings."""
-    employment_income = simulation.baseline_simulation.calculate(
-        "employment_income"
-    )
-    return employment_income[employment_income > 0].median()
