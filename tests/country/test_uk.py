0bb9348d9f3b2e3b498c58497b83e3642a368993
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-05-23 15:24:05 +0100

    Add tests

--- tests/country/test_uk.py
+++ tests/country/test_uk.py
@@ -21,3 +21,45 @@ def test_uk_macro_comparison():
     )
 
     sim.calculate_economy_comparison()
+
+
+def test_uk_macro_bad_package_versions_fail():
+    from policyengine import Simulation
+
+    try:
+        sim = Simulation(
+            scope="macro",
+            country="uk",
+            reform={
+                "gov.hmrc.income_tax.allowances.personal_allowance.amount": 15_000,
+            },
+            package_versions={
+                "policyengine-uk": "-1.0.0",
+            },
+        )
+        raise ValueError(
+            "Simulation should have failed with a bad package version."
+        )
+    except:
+        pass
+
+
+def test_uk_macro_bad_data_versions_fail():
+    from policyengine import Simulation
+
+    try:
+        sim = Simulation(
+            scope="macro",
+            country="uk",
+            reform={
+                "gov.hmrc.income_tax.allowances.personal_allowance.amount": 15_000,
+            },
+            data_versions={
+                "enhanced_frs_2022_23.h5": "-1.0.0",
+            },
+        )
+        raise ValueError(
+            "Simulation should have failed with a bad data version."
+        )
+    except:
+        pass
