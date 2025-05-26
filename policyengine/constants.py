9faeeb2923b667ee997780c98cb4328a191e74fd
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-05-26 19:02:42 +0100

    Add dataset constants

--- policyengine/constants.py
+++ policyengine/constants.py
@@ -4,17 +4,23 @@ from policyengine_core.data import Dataset
 from policyengine.utils.data_download import download
 from typing import Tuple
 
+EFRS_2022 = "gcs://policyengine-uk-data-private/enhanced_frs_2022_23.h5"
+FRS_2022 = "gcs://policyengine-uk-data-private/frs_2022_23.h5"
+CPS_2023_POOLED = "gcs://policyengine-us-data/pooled_3_year_cps_2023.h5"
+CPS_2023 = "gcs://policyengine-us-data/cps_2023.h5"
+ECPS_2024 = "gcs://policyengine-us-data/ecps_2024.h5"
+
 
 def get_default_dataset(
     country: str, region: str, version: str | None = None
 ) -> str:
     if country == "uk":
-        return "gcs://policyengine-uk-data-private/enhanced_frs_2022_23.h5"
+        return EFRS_2022
     elif country == "us":
         if region is not None and region != "us":
-            return "gcs://policyengine-us-data/pooled_3_year_cps_2023.h5"
+            return CPS_2023_POOLED
         else:
-            return "gcs://policyengine-us-data/cps_2023.h5"
+            return CPS_2023
 
     raise ValueError(
         f"Unable to select a default dataset for country {country} and region {region}."
