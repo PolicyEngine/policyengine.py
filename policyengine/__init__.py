d9ff8788bc94ddff3a46d6bcb90d8f568391d3c0
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-08-21 22:09:36 +0100

    Bit of a blank slate

--- policyengine/__init__.py
+++
@@ -1,3 +0,0 @@
-from .simulation import Simulation, SimulationOptions
-
-__version__ = "0.1.1"
--- run.py
+++
@@ -1,8 +0,0 @@
-from policyengine import Simulation
-
-sim = Simulation(
-    country="uk",
-    scope="macro",
-    reform={},
-    model_version="2.0.0",
-)
